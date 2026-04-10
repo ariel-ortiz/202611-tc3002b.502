4ecec577c92aa34ecf530bd89606eb56b0fcce3c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-04-09 19:17:16 -0600

    Update WASM examples: add 'multiply', 'f', and 'f2c' functions

--- Ejemplos WASM/run.py
+++ Ejemplos WASM/run.py
@@ -13,8 +13,14 @@ def call_wasm_fun(file_name, fn_name, *args):
 
 def main():
     print(call_wasm_fun('simple.wat', 'meaning_of_life'))
-    print(call_wasm_fun('simple.wat', 'multiply', 2, 3))
-
+    print(call_wasm_fun('simple.wat', 'multiply', 5, 7))
+    print(call_wasm_fun('simple.wat', 'f', 0))
+    print(call_wasm_fun('simple.wat', 'f', 1))
+    print(call_wasm_fun('simple.wat', 'f', 2))
+    print(call_wasm_fun('simple.wat', 'f', 10))
+    print(call_wasm_fun('simple.wat', 'f2c', 212.0))
+    print(call_wasm_fun('simple.wat', 'f2c', 32.0))
+    print(call_wasm_fun('simple.wat', 'f2c', -40.0))
 
 if __name__ == '__main__':
     main()
--- Ejemplos WASM/simple.wat
+++ Ejemplos WASM/simple.wat
@@ -19,4 +19,30 @@
     local.get $b
     i32.mul
   )
+
+  (func
+    (export "f")
+    (param $x i64)
+    (result i64)
+
+    i64.const 7
+    local.get $x
+    i64.mul
+    i64.const 2
+    i64.add
+  )
+
+  (func
+    (export "f2c")
+    (param $F f64)
+    (result f64)
+
+    local.get $F
+    f64.const 32
+    f64.sub
+    f64.const 5
+    f64.mul
+    f64.const 9
+    f64.div
+  )
 )
